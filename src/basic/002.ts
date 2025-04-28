98c5a276a7343b9b56e43a17a57c5f9f94c8c971
kaike <kaikebartolomeu2003@gmail.com>
2025-04-29 00:49:17 +0100

    feat: terminado exerc.002

---
+++ src/basic/002.ts
@@ -0,0 +1,13 @@
+const PI = 3.1415; // -> Usamos o const usado para declrar valores que serão constantes em todo programa.
+
+function soma() {
+  let n1 = 20;
+  let n2 = 30;
+  let soma = n1 + n2;
+  // -> Usamos o let para declrar variaveis que serão utilizadas dentro de um escopo do nosso programa
+}
+
+var idade = 21
+// -> Usamos o var para declarar variaveis que serão acessadas em qualquer parte do nosso código.
+
+export default {};
